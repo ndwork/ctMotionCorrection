b9e125d1ecfc4922ac4df47a30a0f72fc5bd53bf
ndwork <ndwork@DN51ssi7.SUNet>
2015-02-05 22:50:22 -0800

    translateImg.m add - fully functional

---
+++ translateImg.m
@@ -0,0 +1,45 @@
+function [transImg] = translateImg(img,trans)
+% Function for translating an image in 2D
+% trans = [transY transX]
+  
+  numX = size(img,2);
+  numY = size(img,1);
+
+  % x and y coordinates of original image
+  x = [1:1:numX];
+  y = [1:1:numY];
+  [x y] = meshgrid(x,y);
+  
+  % pad the x coords, y coords, and data
+  padX = [x (numX+1)*ones(size(x,1),1)];
+  padX = [padX; padX(1,:)];
+  
+  padY = [y; (numY+1)*ones(1,size(y,2))];
+  padY = [padY padY(:,1)];
+  
+  padImg = [img img(:,1)];
+  padImg = [padImg; padImg(1,:)];
+  
+  % find x coordinates of translated image
+  transX = x - trans(2);
+  % check for pixels falling off from right
+  tmpX = transX > numX;
+  transX = transX - tmpX*numX;
+  % check for pixels falling off from left
+  tmpX = transX < 1;
+  transX = transX + tmpX*numX;
+  
+  % find y coordinates of translated image
+  transY = y - trans(1);
+  % check for pixels falling off from top
+  tmpY = transY > numY;
+  transY = transY - tmpY*numY;
+  % check for pixels falling off from bottom
+  tmpY = transY < 1;
+  transY = transY + tmpY*numY;
+  
+  % translate image
+  transImg = griddata(padX,padY,padImg,transX,transY);
+
+
+end
