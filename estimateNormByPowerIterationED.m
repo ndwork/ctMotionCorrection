9b66e2df548457961e1eb0f0eb26455e426f8117
ndwork <ndwork@Nicholass-MacBook-Pro-5.local>
2015-02-15 08:16:17 -0800

    Fixed some bugs in estimateNormByPowerIterationED.m.  Cleaned up code a little. Not tested; still probably doesn't work.

--- estimateNormByPowerIterationED.m
+++ estimateNormByPowerIterationED.m
@@ -2,48 +2,41 @@ function [nrm,lambdaVals] = estimateNormByPowerIterationED( ...
   applyE,applyETrans,applyD1,applyD1Trans,applyD2,applyD2Trans,x0,maxIters)
   % x0 is an initial vector to get started with.
 
-  x = x0;
-  
-  if nargin < 4
+  if nargin < 8
     maxIters = 500;
   end
 
   stopTol = 1e-10;
 
+  x = x0;
   lambdaPrev = 0;
-  lambdaVals = [];
+  lambdaVals = zeros(maxIters,1);
 
   for iter = 1:maxIters
-    
-      D1x = applyD1(x);
-      D2x = applyD2(x);
-      D1xTrans = applyD1Trans(x);
-      D2xTrans = applyD2Trans(x);
-      Ex = applyE(x);
-      ExTrans = applyETrans(x);
-      
-      ATransAx = (ExTrans*Ex + D1xTrans*D1x + D2xTrans*D2x) * x;
 
-%      ATransAx = applyATrans(applyA(x));
-     
-     lambda = norm(ATransAx(:));
-     
-     if lambda == 0
-         break
-     end
-     x = ATransAx/lambda;
+    D1x = applyD1(x);
+    D2x = applyD2(x);
+    D1Tx = applyD1Trans(x);
+    D2Tx = applyD2Trans(x);
+    Ex = applyE(x);
+    ETx = applyETrans(x);
 
-     diff = abs(lambda - lambdaPrev);
-     if diff < stopTol
-         break
-     end
+    ATAx = ETx*Ex + D1Tx*D1x + D2Tx*D2x;
 
-     lambdaPrev = lambda;  
-     lambdaVals = [lambdaVals,lambda];
+    lambda = norm(ATAx(:));
+    lambdaVals(iter) = lambda;
 
+    if lambda == 0, break; end;
+
+    x = ATAx / lambda;
+
+    diff = abs(lambda - lambdaPrev);
+    if diff < stopTol, break; end;
+
+    lambdaPrev = lambda;
   end
 
+  lambdaVals = lambdaVals(1:iter);
   nrm = sqrt(lambda);
-
 
 end
