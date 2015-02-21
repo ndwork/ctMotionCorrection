a6eb915d9f59db0ad44f7a27b82d50dbdd502d6a
gsmith9 <gsmith9@stanford.edu>
2015-02-20 21:22:34 -0800

    Updated run_ct by padding the phantom to ensure it stays in the FOV

--- run_ctCorrectForTranslation.m
+++ run_ctCorrectForTranslation.m
@@ -10,17 +10,31 @@ function run_ctCorrectForTranslation
   dTheta = 1 * pi/180;
   thetas = 0:dTheta:pi-dTheta;
   nThetas = numel(thetas);
-
-  maxVerticalShift = 0.01;      % Vertical shift in meters
-  maxHorizontalShift = 0.02;    % Horiztonal shift in meters
-  translations = zeros( nThetas, 2 );
-%   translations(:,1) = linspace(0,maxVerticalShift,nThetas);
-%   translations(:,2) = linspace(0,maxHorizontalShift,nThetas);
-
+  
   % Reconstruction parameters
   cx = 0;   Nx=256;
   cy = 0;   Ny=256;
-  pixSize = 0.001;
+  pixSize = 0.001; % meters / pixel
+
+  maxVerticalShift = 0.01; % in meters
+  maxHorizontalShift = 0.02; % in meters
+  translations = zeros( nThetas, 2 );
+  translations(:,1) = linspace(0,maxVerticalShift,nThetas);
+  translations(:,2) = linspace(0,maxHorizontalShift,nThetas);
+  
+  % pad the phantom image so that it always stays in the field of view
+  xShiftPix = maxHorizontalShift / pixSize;
+  yShiftPix = maxVerticalShift / pixSize;
+  if xShiftPix >= 0 
+    im = [im zeros(size(im,1),ceil(xShiftPix))];
+  else
+    im = [zeros(size(im,1),ceil(abs(xShiftPix))) im];
+  end
+  if yShiftPix >= 0
+    im = [zeros(ceil(yShiftPix),size(im,2)); im];
+  else
+    im = [im; zeros(ceil(abs(yShiftPix)),size(im,2))];
+  end
 
   %sinogram = radonWithTranslation( im, pixSize, nDetectors, detSize, ...
   %  thetas, translations );
@@ -29,5 +43,8 @@ load 'phSinogram.mat'
   recon = ctCorrectForTranslation( sinogram, nDetectors, detSize, ...
     thetas, translations, Nx, Ny, pixSize );
 
+
 end
+
+
 
