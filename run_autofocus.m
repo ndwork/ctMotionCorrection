6cdc6cd7bfb077f4daedd7e88b1e985362e72c17
Karissa Cristiano <karissa-c@att.net>
2015-03-10 01:15:59 -0700

    Updated autofocus

--- run_autofocus.m
+++ run_autofocus.m
@@ -1,41 +1,53 @@
 function [finaltranslation] = run_autofocus
+  clear; close all;
+  addpath(genpath('.'));
+
+  % Reconstruction parameters
   method = 'PC';    % Options: GD, PC, LADMM
-  nRows=32;
-  nCols=32;
-  nDetectors = nCols*2;
+  cy = 0;   nRows=32;
+  cx = 0;   nCols=32;
   pixSize = 0.001; % meters / pixel
-  detSize = 0.001;
-  
-  dTheta = 2 * pi/180;
-  thetas = 0:dTheta:pi-dTheta;
-  nThetas = numel(thetas);
-  
-  %Translation of Image
-  maxHorizontalShift = -0.01;
-  maxVerticalShift = 0.01;
-  
-  translations = zeros( nThetas, 2 );
-  translations(:,1) = linspace(0,maxVerticalShift,nThetas);
-  translations(:,2) = linspace(0,maxHorizontalShift,nThetas);
-  
+
   im = phantom();
   im = imresize( im, [nCols nRows], 'bilinear' );
 
-  figure; imshow( imresize(im,10,'nearest'), [] );  title('original');
-  
+  figure; imshow( imresize(im,10,'nearest'), [] );
+  title('original'); drawnow;
+
+  detSize = 0.001;
+  dTheta = 1 * pi/180;
+  thetas = 0:dTheta:pi-dTheta;
+  nThetas = numel(thetas);
+
+  nDetectors = nCols*2;
+
+  nonzeroTranslations = 1;
+  if nonzeroTranslations > 0
+    maxVerticalShift = 0.002; % in meters
+    maxHorizontalShift = 0.002; % in meters
+    translations = zeros( nThetas, 2 );
+    translations(:,1) = linspace(0,maxVerticalShift,nThetas);
+    translations(:,2) = linspace(0,maxHorizontalShift,nThetas);
+  else
+    maxVerticalShift = 0; % in meters
+    maxHorizontalShift = 0; % in meters
+    translations = zeros( nThetas, 2 );
+  end
+
   im = padImgForRadon( im, maxHorizontalShift, maxVerticalShift, ...
       pixSize );
-  
+  [nRows,nCols] = size(im);
+
   sinogram = radonWithTranslation( im, pixSize, nDetectors, detSize, ...
    thetas, translations );
 
-  minX = -0.01;
-  minY = -0.01;
-  maxX = 0.01;
-  maxY = 0.01;
-  dx = .01;
-  dy = .01;
+  minX = 0;
+  minY = 0;
+  maxX = 0.002;
+  maxY = 0.002;
+  dx = 0.002;
+  dy = 0.002;
 
-  [finaltranslation] = autofocus(sinogram, minX, maxX, dx, minY, maxY, dy, nRows, nCols, nDetectors, pixSize, detSize,thetas, method);
+  [finaltranslation] = autofocus2(sinogram, minX, maxX, dx, minY, maxY, dy, nRows, nCols, nDetectors, pixSize, detSize, thetas, method);
 
 end
